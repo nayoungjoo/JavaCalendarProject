17579db8bf10858d18f0a7fabad8b13f5234a57a
micha1004 <micha1004@DESKTOP-7TG6IAA>
2017-09-04 14:16:14 +0900

    update2

--- src/calendar/CalendarMain.java
+++ src/calendar/CalendarMain.java
@@ -1,10 +1,16 @@
 package calendar;
 
+import java.util.Scanner;
+
 public class CalendarMain {
 
 	public static void main(String[] args) {
 		System.out.println("월화수목금토");
-
+		int a,b;
+		Scanner scannera=new Scanner(System.in);
+		a=scannera.nextInt();
+		b=scannera.nextInt();
+		System.out.println(a+b);
 	}
 
 }
